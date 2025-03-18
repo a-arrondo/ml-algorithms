525160f1595bc4a92159d884472187647b5797f5
a_arrondo <anderarrondoelizburu@gmail.com>
2025-03-18 21:54:34 +0100

    Error fix: determinant equals zero check removed

--- regression/linear_regression/linear_regression.py
+++ regression/linear_regression/linear_regression.py
@@ -9,11 +9,7 @@ class LinearRegression():
         ones_col = np.ones((X.shape[0]))
         X = np.column_stack((ones_col, X))
 
-        if(np.linalg.det(X) != 0):
-            self._coefs = np.linalg.inv(X.T @ X) @ X.T @ y
-        else:
-            print("[ WARNING ] Determinant of the data is zero.\n"
-                    "Coefficients have not been computed.")
+        self._coefs = np.linalg.inv(X.T @ X) @ X.T @ y
 
         return self
 
