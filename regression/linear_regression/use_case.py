e7bcd5b52dcedbb4a721146eac9c6abc777c4c5b
a_arrondo <anderarrondoelizburu@gmail.com>
2025-03-18 13:29:37 +0100

    Visual representation: plot of the data and regression line added

--- regression/linear_regression/use_case.py
+++ regression/linear_regression/use_case.py
@@ -1,6 +1,9 @@
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import seaborn.objects as so
+import matplotlib.pyplot as plt
 from linear_regression import LinearRegression
 
 data_url = "https://raw.githubusercontent.com/YBI-Foundation/Dataset/refs/heads/main/Salary%20Data.csv"
@@ -19,4 +22,19 @@ lm = lm.fit(X.values, y.values)
 
 print("PREDICTION")
 print(lm.predict(np.matrix(10)))
+
+inp = np.matrix(np.linspace(np.min(X), np.max(X), num = salaries.shape[0])).T
+output = lm.predict(inp)
+
+sns.scatterplot(salaries, x="Experience Years", y="Salary")
+plt.plot(inp, output.T, color = "red")
+
+plt.title("Linear regression")
+plt.xlabel("Experience Years")
+plt.ylabel("Salary")
+
+plt.show()
+
+# sns.scatterplot(data=salaries, x="Experience Years", y="Salary")
+# plt.show()
 
