19e7c947294644973555083190175f60330f47a7
a_arrondo <anderarrondoelizburu@gmail.com>
2025-03-18 13:28:41 +0100

    Linear regression: __init__ function and determinant equals zero check added

--- regression/linear_regression/linear_regression.py
+++ regression/linear_regression/linear_regression.py
@@ -2,12 +2,18 @@
 import numpy as np
 
 class LinearRegression():
+    def __init__(self):
+        self._coefs = None
 
     def fit(self, X, y):
         ones_col = np.ones((X.shape[0]))
         X = np.column_stack((ones_col, X))
 
-        self._coefs = np.linalg.inv(X.T @ X) @ X.T @ y
+        if(np.linalg.det(X) != 0):
+            self._coefs = np.linalg.inv(X.T @ X) @ X.T @ y
+        else:
+            print("[ WARNING ] Determinant of the data is zero.\n"
+                    "Coefficients have not been computed.")
 
         return self
 
