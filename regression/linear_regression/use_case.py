a2118d7e6abea9a58a038bf82e5ce8595c1c0e6f
a_arrondo <anderarrondoelizburu@gmail.com>
2025-03-19 17:25:50 +0100

    Data visualization: plot aesthetics changed

--- regression/linear_regression/use_case.py
+++ regression/linear_regression/use_case.py
@@ -12,6 +12,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, \
 data_url = "https://raw.githubusercontent.com/YBI-Foundation/Dataset/refs/heads/main/Salary%20Data.csv"
 df = pd.read_csv(data_url)
 
+
 # DATA EXPLORATION
 print(f"# Dimensionality")
 print(f"({df.shape[0]}, {df.shape[1]})")
@@ -26,13 +27,13 @@ SALARY_COL = df.columns[1]
 X = df[YEAR_COL].values
 y = df[SALARY_COL].values
 
+
 # MODEL EVALUATION
 X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=1)
 
 lm = LinearRegression()
 lm = lm.fit(X_train, y_train)
-print(X_test)
 y_pred = np.asarray(lm.predict(X_test))
 
 mae = mean_absolute_error(y_test, y_pred)
@@ -41,13 +42,13 @@ rmse = mean_squared_error(y_test, y_pred, squared = False)
 exp_var = explained_variance_score(y_test, y_pred)
 r_squared = r2_score(y_test, y_pred)
 
-
 print("\n# Prediction statistics")
 print(f"\tMAE: {mae}")
 print(f"\tMSE: {mse}")
 print(f"\tRMSE: {rmse}")
 print(f"\tExplained variance: {exp_var}")
 print(f"\tR2 score: {r_squared}")
+
 
 # DATA VISUALIZATION
 lm = LinearRegression()
@@ -57,12 +58,18 @@ reg_inp = np.array(np.linspace(np.min(X), np.max(X),
     num = df.shape[0]))
 reg_out = lm.predict(reg_inp)
 
-sns.scatterplot(df, x=YEAR_COL, y=SALARY_COL)
-plt.plot(reg_inp, reg_out.T, color = "red")
+sns.scatterplot(df, x=YEAR_COL, y=SALARY_COL, alpha=0.8, s = 50)
+plt.plot(reg_inp, reg_out.T, color = "red", alpha=0.8)
 
-plt.title("Linear regression")
-plt.xlabel(YEAR_COL)
-plt.ylabel(SALARY_COL)
+equation = f"Salary = {lm._coefs[1]:.3f} * Experience + {lm._coefs[0]:.3f}"
+plt.text(1, 120000, equation, fontsize=14, color="red")
+
+plt.title("Salary vs. Years of Experience:\n"
+        "(Univariate Linear Regression)",
+        fontweight="bold", fontsize=15)
+
+plt.xlabel(YEAR_COL, fontsize=14)
+plt.ylabel(SALARY_COL, fontsize=14)
 
 plt.show()
 
