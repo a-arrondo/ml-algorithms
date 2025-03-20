a3c59f6c15b7d97c59b7ee52385d84ed7cf69495
a_arrondo <anderarrondoelizburu@gmail.com>
2025-03-20 20:16:09 +0100

    Documentation: inline documentation of the class methods added

--- regression/linear_regression/linear_regression.py
+++ regression/linear_regression/linear_regression.py
@@ -17,11 +17,11 @@ class LinearRegression():
     
     Methods
     -------
-    fit(X, y)
+    fit(X, y, l2_penalty=0)
         Computes the coefficients to fit the provided data.
     predict(X)
         Returns the predicted values of the given values.
-    fit_predict(X, y)
+    fit_predict(X, y, l2_penalty=0)
         First, fits the model, and then returns the predictions of the training data.
     get_intercept()
         Returns the intercept of the linear model.
@@ -38,6 +38,22 @@ class LinearRegression():
         self.l2_penalty_ = 0
 
     def fit(self, X, y, l2_penalty=0):
+        """
+        Computes the coefficients to fit the provided data.
+
+        The parameter 'l2_penalty' may be adjusted to apply ridge regression.
+        By default, it is disabled: l2_penalty = 0.
+
+        Returns
+        -------
+        The fitted linear model object.
+
+        Raises
+        ------
+        ValueError
+            If parameter y is 1D, or if the inverse cannot be computed
+            due to a singular matrix.
+        """
         if not isinstance(X, np.ndarray):
             X = np.array(X)
         if not isinstance(y, np.ndarray):
@@ -60,10 +76,17 @@ class LinearRegression():
         return self
 
     def predict(self, X):
+        """
+        Calculates the predictions of the given input values.
+
+        Returns
+        -------
+        A 1D NumPy array with the predictions.
+        """
         if not isinstance(X, np.ndarray):
             X = np.array(X)
         if X.ndim != 2:
-            X = X.reshape(len(X), 1)
+            X = X.reshape(len(X), 1))
 
         ones_col = np.ones(X.shape[0])
         X = np.column_stack((ones_col, X))
@@ -71,12 +94,34 @@ class LinearRegression():
         return X @ self.coefs_
 
     def fit_predict(self, X, y, l2_penalty=0):
+        """
+        First, fits a model, and then predicts the training values.
+
+        The parameter 'l2_penalty' may be adjusted to apply ridge regression.
+        By default, it is disabled: l2_penalty = 0.
+
+        Raises
+        ------
+        ValueError
+            If parameter y is 1D, or if the inverse cannot be computed
+            due to a singular matrix.
+
+        Returns
+        -------
+        A 1D NumPy array with the predictions.
+        """
         self = self.fit(X, y, l2_penalty)
         return self.predict(X)
 
     def get_intercept(self):
+        """
+        Returns the intercept of the linear model.
+        """
         return self.coefs_[1]
 
     def get_coefs(self):
+        """
+        Returns the coefficients of the linear model.
+        """
         return self.coefs_[1:]
 
